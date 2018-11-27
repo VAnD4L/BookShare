17c147f454d92f48e6e894ce28faf15fd2f69505
VAnD4L <orenzelig@gmail.com>
2018-11-27 02:50:50 +0200

    changed cover images directory location again and fixed display bug

--- app/src/main/java/com/example/bookshare/bookshare/MainActivity.java
+++ app/src/main/java/com/example/bookshare/bookshare/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         // add a function to add the 4 latest read books's covers as image buttons for new_books_slider
         // + make the slider dynamic size
         // add a function to add the 4 latest active friends's profile photos as image buttons for last_friends_slider
