258101f0b4869005ad8896ca2189c78b30d61284
VAnD4L <orenzelig@gmail.com>
2018-11-30 17:38:03 +0200

    added action bar in main activity with a toggle for nav menu

--- app/src/main/java/com/example/bookshare/bookshare/MainActivity.java
+++ app/src/main/java/com/example/bookshare/bookshare/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.bookshare.bookshare;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -15,22 +17,35 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     private RecyclerView postList;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // add a function to add the 4 latest read books's covers as image buttons for new_books_slider
-        // + make the slider dynamic size
-        // add a function to add the 4 latest active friends's profile photos as image buttons for last_friends_slider
-        // + make the slider dynamic size
         setContentView(R.layout.activity_main);
-        drawerLayout =  findViewById(R.id.drawable_layout);
+
+
+
+        mToolbar = findViewById(R.id.main_page_toolbar);// add the toolbar
+        setSupportActionBar(mToolbar);
+        //getSupportActionBar().setTitle("home"); // moved to layout.xml
+
+
+
+        drawerLayout =  findViewById(R.id.drawable_layout); // toggle button for the nav menu
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
         navigationView =  findViewById(R.id.navigation_view);
         View navView =  navigationView.inflateHeaderView(R.layout.navigation_header); // TODO add imageButton to link into profile
 
-
+        // listener for when pressing an item on the nav menu
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -40,7 +55,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void UserMenuSelector(MenuItem item){
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private void UserMenuSelector(MenuItem item){ // returnes to nav menu item press listener
         switch (item.getItemId())
         {
             case R.id.nav_Post:
